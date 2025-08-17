1d74be6c803231b0b0cdebdc660452fa4a40d5e3
juntoku <jun.toku97@gmail.com>
2025-08-16 21:41:44 -0400

    setup

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
+import logging
 from hyperliquid.info import Info  
 from hyperliquid.exchange import Exchange
 from hyperliquid.info import Info
 import example_utils
 from hyperliquid.utils import constants
 import time   
-# Or explicitly enable WebSocket  
+
+# Set log level to DEBUG
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
 def callback(msg):  
-    print("callback")
-    print(f"Received: {msg}")  
+    logger.debug("callback")
+    logger.debug(f"Received: {msg}")  
 
 
 address, info, exchange = example_utils.setup(base_url=constants.MAINNET_API_URL, skip_ws=False)
@@ -15,29 +20,22 @@ address, info, exchange = example_utils.setup(base_url=constants.MAINNET_API_URL
 # Subscribe with the callback  
 subscription = {"type": "allMids"}
 
-
 while True:
-    result= info.subscribe(subscription, callback)
-    print(result)
-    print("subscribed")
-    print(info.ws_manager.active_subscriptions)
-    print(info.ws_manager.ws_ready)
+    result = info.subscribe(subscription, callback)
+    logger.debug(f"Subscribe result: {result}")
+    logger.debug("subscribed")
+    logger.debug(f"Active subscriptions: {info.ws_manager.active_subscriptions}")
+    logger.debug(f"WS ready: {info.ws_manager.ws_ready}")
     time.sleep(1)
 
-
-
-
-
-
 # user_state = info.user_state("0xB6001dDB4ecf684A226361812476f731CEA96d05")
-# print(user_state)
+# logger.debug(user_state)
 
 # Place an order that should rest by setting the price very low
 # order_result = exchange.order("ETH", True, 0.2, 1100, {"limit": {"tif": "Gtc"}})
-# print(order_result)
+# logger.debug(order_result)
 
 # open_orders = info.open_orders(address)
 # for open_order in open_orders:
-#     print(f"cancelling order {open_order}")
+#     logger.debug(f"cancelling order {open_order}")
 #     exchange.cancel(open_order["coin"], open_order["oid"])
-
